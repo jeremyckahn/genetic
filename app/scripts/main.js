4461a0d37e50281c3f4e649e54ddd0eaaf12da19
Jeremy Kahn <jeremyckahn@gmail.com>
2015-12-23 11:32:40 -0600

    Update to Lateralus 0.9.3

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,7 +21,7 @@ require.config({
     text: 'bower_components/requirejs-text/text'
     ,jquery: 'bower_components/jquery/dist/jquery'
     ,backbone: 'bower_components/backbone/backbone'
-    ,lodash: 'bower_components/lodash/dist/lodash'
+    ,lodash: 'bower_components/lodash/lodash'
     ,mustache: 'bower_components/mustache/mustache'
     ,processing: 'bower_components/Processing.js/processing'
     ,seedrandom: 'bower_components/seedrandom/seedrandom'
